37f87de45092445c8130a022bc08c2221df1397b
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-01-28 16:48:26 +0200

    First app

---
+++ 1_First_Application/main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("1_First_Application", "Main");
+
+    return app.exec();
+}
